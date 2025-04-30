d4fd07e2fa39fc3fd2feba1c37e2c23a84d693f4
Addel12 <102005412+Addel12@users.noreply.github.com>
2025-05-01 00:29:02 +0700

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from detector import app  # Mengimpor aplikasi Flask dari detector.py
+import os
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
